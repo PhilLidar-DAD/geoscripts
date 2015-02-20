fefbf98803179408e37b004df8c51660310b9b91
Ken Salanio <kssalanio@gmail.com>
2015-02-20 16:15:18 +0800

    finale revision for wfs_test.py

--- ceph/geoserver/wfs_test.py
+++ ceph/geoserver/wfs_test.py
@@ -5,13 +5,14 @@ from pprint import pprint
 
 from owslib.wfs import WebFeatureService
 from urlparse import urlparse
+from owslib.etree import etree
 
+#Retrieve WFS XML
 wfs_version="1.0.0"
 username="admin"
 password="geoserver"
 method="Get"
 base_url="http://192.168.56.52:8080/geoserver/wfs"
-
 
 request = { "version"       : wfs_version, 
             "request"       : "GetFeature",
@@ -21,14 +22,16 @@ request = { "version"       : wfs_version,
             }
 
 data = urlencode(request)
-
-
-
-#pprint(data)
 u = openURL(base_url, data, method, username = username, password = password)
-print "Got a file of type: "+u.info().gettype()
+
+#Create lxml tree and namespace map
 wfs_xml = u.read()
+wfs_etree = etree.fromstring(wfs_xml)
+wfs_nsmap = wfs_etree.nsmap
+wfs_nsmap.pop(None,None)
 
-wfs = WebFeatureService(base_url, xml=wfs_xml, version=wfs_version)
+#Get a list of gridrefs using xpath
+gridrefs = wfs_etree.xpath("//geonode:GRID_REF/text()",
+                            namespaces=wfs_nsmap)
 
-pprint(sorted(wfs.contents.keys()))
+pprint(gridrefs)
