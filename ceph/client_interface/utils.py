3c1b053c1b952f93bd9c976c17928b986327412a
Ken Salanio <kssalanio@gmail.com>
2015-02-24 13:15:48 +0800

    [CEPH] modified utils.py

--- ceph/client_interface/utils.py
+++ ceph/client_interface/utils.py
@@ -6,8 +6,10 @@ def list_all_files_in_subdirs(root_dir):
     file_list = []
     for path, subdirs, files in walk(files_source_dir):
         for name in files:
+            grid_ref = path.rsplit("/")[-1]
             file_list.append(join(path, name))
+    return file_list
 
 def get_gridref_from_fpath(fpath):
-    
+    return fpath.rsplit("/")[-1]
 
