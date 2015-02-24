d9ff25cd7c7855caf095dcd0cb7c5bd6f37fd124
Ken Salanio <kssalanio@gmail.com>
2015-02-24 14:35:03 +0800

    [CEPH] Added a test script for deleting files inside a swift container

--- ceph/tests/test_del_files_swift.py
+++ ceph/tests/test_del_files_swift.py
@@ -29,50 +29,7 @@ try:
     print "====================="
 
     container_name = 'data-container'
-    conn.put_container(container_name)
-    
-    print "\n Listing Containers AGAIN"
-    print "====================="
-    for container in conn.get_account()[1]:
-        print container['name']
-        
-    print "====================="
-    
-    path_to_folder="/root/Deviantart/"
-    file_list = [ f for f in listdir(path_to_folder) if isfile(join(path_to_folder,f)) ]
-    
-    print "\nUploading files inside directory [{0}]:".format(path_to_folder)
-    print "=========================="
-    for f in file_list:
-        file_path=str(path_to_folder)+str(f)
-        
-        content_type=None
-        try:
-            content_type = mimetypes.types_map["."+f.split(".")[-1]]
-        
-        
-        except KeyError:
-            pass
-        
-        print "Uploading  file {0} [size:{1} | type:{2}]...".format(   f,
-                                                            os.stat(file_path).st_size,
-                                                            content_type)
-        ###
-        #  next version of swiftclient?
-        ###
-        #~ #obj = obj_container.create_object(f)
-        #~ #obj.content_type = 'text/plain'
-        #~ #obj.load_from_filename(file_path)
-        
-        with open(file_path, 'r') as file_obj:
-            conn.put_object(container_name, f,
-                                            contents=file_obj.read(),
-                                            content_type=content_type)
-    print "=========================="
-    
-    obj_container = conn.get_container(container_name)
-    #~ pprint(obj_container)
-    
+     
     print "\nList of Files inside container '{0}':".format(container_name)
     print "=========================="
     output_folder="/root/out/"
